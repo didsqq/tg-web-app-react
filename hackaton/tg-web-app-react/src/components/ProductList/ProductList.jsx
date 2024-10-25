6e0597b7de02000566655b23831fb5e56178691e
adel <adelgaripov6@gmail.com>
2024-10-25 19:29:41 +0300

    3

---
+++ hackaton/tg-web-app-react/src/components/ProductList/ProductList.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import "./ProductList.css";
+const ProductList = () => {
+    return (
+        <div>
+            ProductListtt
+        </div>
+    );
+};
+
+export default ProductList;
