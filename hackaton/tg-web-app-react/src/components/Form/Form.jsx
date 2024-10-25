f5a6f4a81862455b1763f382c82d4c8d618faeea
adel <adelgaripov6@gmail.com>
2024-10-25 22:56:42 +0300

    123

--- hackaton/tg-web-app-react/src/components/Form/Form.jsx
+++ hackaton/tg-web-app-react/src/components/Form/Form.jsx
@@ -15,7 +15,7 @@ const Form = () => {
             subject
         }
         tg.sendData(JSON.stringify(data));
-    }, [country, city, subject])
+    }, [country, street, subject])
 
     useEffect(() => {
         tg.onEvent(onSendData)
